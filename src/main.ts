431c225b981675c96afd7438f6e7b2090e5368dd
Platto Artyom <platar.ant@yandex.ru>
2025-02-21 05:29:40 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 import App from './App.vue'
 import router from './router'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives
+})
+
 app.use(createPinia())
-app.use(router)
+  .use(router)
+  .use(vuetify)
+// Vue.use(BootstrapVue)
+// Vue.use(BootstrapVueIcons)
 
 app.mount('#app')
